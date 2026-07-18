0916d3b43b3f561f475bd0d5a15dde8739da45c8
Anezium <anezium@gmail.com>
2026-07-18 19:37:44 +0200

    Cut v1.0.16 of the phone and glasses apps

--- glasses-hub/build.gradle.kts
+++ glasses-hub/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.anezium.rokidbus.glasses"
         minSdk = 31
         targetSdk = 32
-        versionCode = 10015
-        versionName = "1.0.15"
+        versionCode = 10016
+        versionName = "1.0.16"
     }
 
     buildTypes {
